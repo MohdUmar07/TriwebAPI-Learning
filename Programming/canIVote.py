543204e271cebc73e0e95a3c4d6fa7033aa8163e
MohdUmar07 <umohd9554@gmail.com>
2024-05-23 21:31:44 +0530

    Implement conditional logic using if/else statements

---
+++ Programming/canIVote.py
@@ -0,0 +1,10 @@
+# PRogram for checking that you are eligible to vote or not.
+
+print("Enter your age to see you are eligible to vote or not");
+
+age = int(input("Age "));
+
+if age >= 18:
+    print("You are eligible to vote");
+else:
+    print("You are not eligible to vote")
---
+++ Programming/comparison.py
@@ -0,0 +1,12 @@
+# Comparison between two person, who is taller?
+
+person1 = int(input("Hight of first person  is "));
+person2 = int(input("Hight of second person  is "));
+
+if person1 > person2:
+    print("Person one is taller");
+else:
+    if person2 > person1:
+        print("person two is taller");
+    else:
+        print("Both are equal")
