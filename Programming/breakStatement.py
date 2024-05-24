07ff8cd335e63b06f909d9da868b2c676d9974b9
MohdUmar07 <umohd9554@gmail.com>
2024-05-24 16:58:32 +0530

    implements break and continue statements.

---
+++ Programming/breakStatement.py
@@ -0,0 +1,9 @@
+
+for i in range(10):
+    print("Inside loop before condition")
+    if i == 5:
+        print("Inside if condition")
+        break
+    print("After if condition")
+    print(i);
+print("After loop")    
