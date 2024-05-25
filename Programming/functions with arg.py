7c281fed62670acb9bb1d442e35dcc2936c9aa52
MohdUmar07 <umohd9554@gmail.com>
2024-05-25 15:59:08 +0530

    Implemented functions with or without arguments.

---
+++ Programming/functions with arg.py
@@ -0,0 +1,9 @@
+def addTowNumbers(num1, num2):
+    sum = num1 + num2
+    # print(sum)
+    return sum
+
+# addTowNumbers(40,10)
+
+result = addTowNumbers(40,10) 
+print(result)   
---
+++ Programming/functions without arg.py
@@ -0,0 +1,9 @@
+def someFunction():
+    print("In the function")
+    print("In the function 1")
+    print("In the function 2")
+    print("In the function 3")
+    
+print("Program is runnning")
+someFunction()
+print("After the function")
