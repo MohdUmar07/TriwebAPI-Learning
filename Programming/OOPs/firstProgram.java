63bb17fcb2e1b5992fadb263f2611cf4626b3b6d
MohdUmar07 <umohd9554@gmail.com>
2024-05-26 13:28:19 +0530

    Implemented first program in java.

---
+++ Programming/OOPs/firstProgram.java
@@ -0,0 +1,18 @@
+class xyz{
+	public static int area(int a, int b){
+		return a*b;
+}
+
+	public static int area(int a, int b, int c){
+		return a*b*c;
+}
+	
+	public static void main(String[] arg){
+		
+		System.out.println("Hello Wrold");
+		System.out.println("Area is "+area(2,5));
+		System.out.println("Area is "+area(2,5,4));
+		
+	}
+
+}
