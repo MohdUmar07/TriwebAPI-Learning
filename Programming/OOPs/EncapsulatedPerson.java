27b8679cc8ed36e00596d1b695ece319a7be7547
MohdUmar07 <umohd9554@gmail.com>
2024-05-28 14:10:32 +0530

    Practiced the concepts of Encapsulation in Java.

---
+++ Programming/OOPs/EncapsulatedPerson.java
@@ -0,0 +1,13 @@
+// Encapsulation 
+public class EncapsulatedPerson {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+}
---
+++ Programming/OOPs/Encapsulation.java
@@ -0,0 +1,10 @@
+// Encapsulation 
+public class Encapsulation {
+    public static void main(String[] args) {
+        EncapsulatedPerson person1 = new EncapsulatedPerson();
+        person1.setName("Alice");
+
+        System.out.println("Name: " + person1.getName());
+
+    }
+}
