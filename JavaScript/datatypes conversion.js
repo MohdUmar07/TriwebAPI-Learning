50088b8003b64d8a5c9b75b3b96fb64e37500d64
MohdUmar07 <umohd9554@gmail.com>
2024-06-10 23:03:08 +0530

    Datatypes and its conversion in JS

---
+++ JavaScript/datatypes conversion.js
@@ -0,0 +1,33 @@
+let n = 1;
+console.log(typeof(n));//number
+
+n = "Hi Anas"//?
+console.log(typeof(n));//number
+
+//Note; typescript
+
+let num1 = '3';
+let num2 = 5;
+
+console.log(num1+num2);// string and + perform concatination 
+
+let num3 = '3';
+let num4 = 5;
+console.log(parseInt(num3)+num4); 
+
+let num5 = '3.5';
+let num6 = 5;
+console.log(parseFloat(num5)+num6); 
+
+let num7 = '3';
+let num8 = 5;
+console.log(+num7+num8);
+
+
+console.log(-num7+num8); 
+console.log(num7-num8); 
+console.log(num7*num8);
+console.log(num7/num8);
+
+n9 = '101';
+console.log(parseInt(n9, 2)); 
---
+++ JavaScript/datatypes.js
@@ -0,0 +1,15 @@
+let nullVaribale = null;
+
+let undefinedVariable;
+
+let numVar = 12 ;
+
+let str = "Hi hello";
+
+let obj = {};
+
+console.log("null", nullVaribale, "", typeof(nullVaribale));
+console.log("undefinedVariable", undefinedVariable, "", typeof(undefinedVariable));
+console.log("numVar", numVar, "", typeof(numVar));
+console.log("nustrll", str, "", typeof(str));
+console.log("obj", obj, "", typeof(obj));
