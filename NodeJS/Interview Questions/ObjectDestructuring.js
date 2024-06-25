cf5becbfdf7813cee90781761901efa58ce7978a
MohdUmar07 <umohd9554@gmail.com>
2024-06-25 19:45:41 +0530

    Practiced Object Destructuring

---
+++ NodeJS/Interview Questions/ObjectDestructuring.js
@@ -0,0 +1,10 @@
+const student = {
+    "sname":"Aban",
+    "mobile":984544,
+    "email":"ans@gmail.com"
+};
+
+const {sname, mobile, email} = student;
+console.log(`Name:${sname}
+Mobile:${mobile}
+Email:${email}`);
