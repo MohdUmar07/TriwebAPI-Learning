64c8e3a771cc4831e1837983e72930f93b7d98cc
MohdUmar07 <umohd9554@gmail.com>
2024-06-25 12:40:07 +0530

    Practiced arithmetic operations in NodeJS

---
+++ NodeJS/Programming from Scratch/ArithmeticOperations.js
@@ -0,0 +1,22 @@
+const num1 = 5;
+const num2 = 2;
+
+const sum = num1 + num2;
+console.log("Sum is", sum);
+
+
+const diff = num1 - num2;
+console.log("Difference is", diff);
+
+
+const prod = num1 * num2;
+console.log("Product is", prod);
+
+
+const quotient = num1 / num2;
+const quotientWithRemainder = Math.floor(num1 / num2);
+const remainder = num1 % num2;
+
+console.log("Division without remainder", quotient);
+console.log("Division with remainder", quotientWithRemainder);
+console.log("Remainder is", remainder);
