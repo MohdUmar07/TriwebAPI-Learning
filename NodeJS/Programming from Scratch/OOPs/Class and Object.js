13f5546df33360a4735c0603c991f086e3c77cb2
MohdUmar07 <umohd9554@gmail.com>
2024-06-25 13:40:23 +0530

    Practiced Class and Object in NodeJS

---
+++ NodeJS/Programming from Scratch/OOPs/Class and Object.js
@@ -0,0 +1,13 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+}
+
+const person1 = new Person('Anas', 20);
+person1.greet();
