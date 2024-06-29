0290c92f39b81025555e19a1d2672d78e510659f
MohdUmar07 <umohd9554@gmail.com>
2024-06-29 15:06:21 +0530

    Added the function to calculate and declare the winner

--- Projects/tic-tac-toe/src/App.js
+++ Projects/tic-tac-toe/src/App.js
@@ -1,26 +1,35 @@
 import { useState } from "react";
 import Square from "./square";
+import calculateWinner from "./calculateWinner"
 
 function Board() {
   const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if(calculateWinner(squares) || squares[i]){
       return;
     }
     const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
+    if (xIsNext){
+      nextSquares[i] = "X";
     } else {
-      nextSquares[i] = 'O';
+      nextSquares[i] = "O";
     }
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
 
   return (
     <div className="board">
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
