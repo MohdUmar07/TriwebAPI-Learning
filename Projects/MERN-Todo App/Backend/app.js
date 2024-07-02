3bc5bdbbd1377207a7c19fb1970e5016e0bb9952
MohdUmar07 <umohd9554@gmail.com>
2024-07-02 17:37:58 +0530

    added CORS support to Express server

--- Projects/MERN-Todo App/Backend/app.js
+++ Projects/MERN-Todo App/Backend/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const todoRouter = require('./routers/todo');
 
 const app = express()
 app.use(express.json());
+app.use(cors());
 
 app.use('/', todoRouter);
 
@@ -14,7 +16,7 @@ mongoose.connect(uri, {
     serverSelectionTimeoutMS: 5000
 }).catch(err => console.log(err.reason));
 
-app.listen(3000, (err) => {
+app.listen(3002, (err) => {
     if (err) {
         console.log(err);
     }
